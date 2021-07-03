ef643be82a0f2c527ffc5ee3be4b53c5f9b7f30e
Firtch <firtch@gmail.com>
2021-07-03 06:07:33 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Modal from "./Modal";
+import RandomUser from "./RandomUser";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+export default class App extends React.Component {
+
+  state = {
+    modalVisible: false,
+  };
+
+  showModal = (_) => this.setState((_) => ({ modalVisible: true }));
+  hideModal = (_) => this.setState((_) => ({ modalVisible: false }));
+
+  render() {
+    const { modalVisible } = this.state;
+
+    return (
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+        <button onClick={this.showModal}>Show modal</button>
+
+        {modalVisible && (
+          <Modal closeHandler={this.hideModal}>
+            <RandomUser />
+          </Modal>
+        )}
+      </div>
+    );
+  }
+}
